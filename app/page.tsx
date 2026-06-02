267ab733dc43b408123dcdad8c4c64cb46c61b29
azizu06 <ab725492@ucf.edu>
2026-06-02 13:06:13 -0400

    Save preference form WIP

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 import { useState } from "react";
-import StartForm from "./components/startForm";
+import SessionForm from "./components/sessionForm";
+import PrefForm from "./components/prefForm";
 import { Session, Preferences, Movie } from "./types";
-import next from "next";
 export default function Home() {
   const [step, setStep] = useState("setup");
   const [session, setSession] = useState<Session | null>(null);
@@ -15,17 +15,24 @@ export default function Home() {
     setStep("prefs");
   };
   const handlePrefs = (data: Preferences) => {
+    if (!session) return;
     const nextIdx = prefIdx + 1;
     setPrefs((prev) => [...prev, data]);
     setPrefIdx((prev) => prev + 1);
-    if (nextIdx == session?.peopleCount) setStep("results");
+    if (nextIdx == session.peopleCount + 1) setStep("results");
   };
   const handleMovies = (data: Movie[]) => setRecs(data);
   const nextMovie = () => setRecIdx((prev) => prev + 1);
   return (
     <>
-      {step === "setup" && <StartForm />}
-      {step == "prefs" && <PrefForm step={step} prefIdx={prefIdx} />}
+      {step === "setup" && <SessionForm handleSubmit={handleSetup} />}
+      {step == "prefs" && (
+        <PrefForm
+          handleSubmit={handlePrefs}
+          isLast={prefIdx === session?.peopleCount}
+          prefIdx={prefIdx}
+        />
+      )}
       {step === "results" && (
         <Movie nextMovie={nextMovie} movie={recs[recIdx]} />
       )}
